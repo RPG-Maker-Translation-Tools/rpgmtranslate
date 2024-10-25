eab91f806db998a04236d9256c24337f3ce580e8
savannstm <savannstm@gmail.com>
2024-10-26 00:13:00 +0700

    Add new invoke functions

--- src/extensions/invokes.ts
+++ src/extensions/invokes.ts
@@ -1,4 +1,5 @@
 import { invoke } from "@tauri-apps/api/core";
+import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
 
 export async function invokeRead(args: ReadCommandOptions) {
     await invoke("read", args);
@@ -14,4 +15,12 @@ export async function invokeEscapeText(args: { text: string }): Promise<string> 
 
 export async function invokeReadLastLine(args: { filePath: string }): Promise<string> {
     return await invoke<string>("read_last_line", args);
+}
+
+export async function invokeAddToScope(args: { window: WebviewWindow; path: string }) {
+    await invoke("add_to_scope", args);
+}
+
+export async function invokeTranslateText(args: { text: string; to: "ru"; from: "en" }): Promise<string> {
+    return await invoke<string>("translate_text", args);
 }
