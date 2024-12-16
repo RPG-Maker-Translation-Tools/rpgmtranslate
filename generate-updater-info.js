84be594a057ec670e4022e9066f845aeaf887f3a
savannstm <savannstm@gmail.com>
2024-12-16 18:10:28 +0700

    Fix script producing incorrect urls to program download

--- generate-updater-info.js
+++ generate-updater-info.js
@@ -20,13 +20,13 @@ const updaterMeta = {
 if (await windowsSignatureFile.exists()) {
     updaterMeta.platforms["windows-x86_64"].signature = await windowsSignatureFile.text();
     updaterMeta.platforms["windows-x86_64"].url =
-        `https://github.com/savannstm/rpgmtranslate/releases/download/${version}/rpgmtranslate_${version}_x64_en-US.msi`;
+        `https://github.com/savannstm/rpgmtranslate/releases/download/v${version}/rpgmtranslate_${version}_x64_en-US.msi`;
 }
 
 if (await linuxSignatureFile.exists()) {
     updaterMeta.platforms["linux-x86_64"].signature = await linuxSignatureFile.text();
     updaterMeta.platforms["linux-x86_64"].url =
-        `https://github.com/savannstm/rpgmtranslate/releases/download/${version}/rpgmtranslate_${version}_amd64.AppImage`;
+        `https://github.com/savannstm/rpgmtranslate/releases/download/v${version}/rpgmtranslate_${version}_amd64.AppImage`;
 }
 
 await Bun.write("./src-tauri/target/release/bundle/updater_info.json", JSON.stringify(updaterMeta));
